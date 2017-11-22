71142d63b1032892658c6e7dd98909afd0b8462d
kallimachos <kallimachos@gmail.com>
2017-11-22 11:04:45 +1000

    Update git-guide and tidy requirements

--- doc/conf.py
+++ doc/conf.py
@@ -157,7 +157,8 @@ html_context = {
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
-html_use_smartypants = False
+# This option has been deprecated.
+# html_use_smartypants = False
 
 # Custom sidebar templates, maps document names to template names.
 # html_sidebars = {}
